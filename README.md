154ab5d29376dbebf284a8c37cbced8d4deb02a6
Simone <xsimone97@hotmail.it>
2023-08-12 00:45:19 +0200

    only country

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Free and open source ip to geo api
 
 convert an ip to its corresponding country :it: with a **GET** request to `/api/country/{ip}` as in this [example](https://ip2geo.deno.dev/api/country/8.8.8.8).
 
-### Ip to City
+<!-- ### Ip to City
 
 convert an ip to its corresponding city :statue_of_liberty: with a **GET** request to `/api/city/{ip}` as in this [example](https://ip2geo.deno.dev/api/city/8.8.8.8).
 
 ### Ip to ASN
 
-convert an ip to its corresponding ASN :1234: with a **GET** request to `/api/asn/{ip}` as in this [example](https://ip2geo.deno.dev/api/asn/8.8.8.8).
+convert an ip to its corresponding ASN :1234: with a **GET** request to `/api/asn/{ip}` as in this [example](https://ip2geo.deno.dev/api/asn/8.8.8.8). -->
 
 ## Dev
 
--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import maxmind from "https://esm.sh/maxmind@0.6.0";
 
 const countryDbPath = "./db/GeoIP.dat";
-const cityDbPath = "./db/GeoIPCity.dat";
-const asnDbPath = "./db/GeoIPASNum.dat";
+// const cityDbPath = "./db/GeoIPCity.dat";
+// const asnDbPath = "./db/GeoIPASNum.dat";
 const isIpv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
 // const isIpv6Regex = /^([\da-fA-F]{1,4}:){7}[\da-fA-F]{1,4}$/;
 
-maxmind.init([countryDbPath, cityDbPath, asnDbPath]);
+maxmind.init([countryDbPath]);
+// loading all db is too slow for deno deploy
+// maxmind.init([countryDbPath, cityDbPath, asnDbPath]);
 
 const router = new Router();
 router
@@ -24,27 +26,27 @@ router
         "status": "error",
       }
     }
-  })
-  // ip 2 city ipv4
-  .get("/api/city/:ip", async (context) => {
-    if (context?.params?.ip && isIpv4Regex.test(context?.params?.ip)) {
-      context.response.body = await maxmind.getLocation(context?.params?.ip);
-    } else {
-      context.response.body = {
-        "status": "error",
-      }
-    }
-  })
-  // ip 2 asn ipv4
-  .get("/api/asn/:ip", async (context) => {
-    if (context?.params?.ip && isIpv4Regex.test(context?.params?.ip)) {
-      context.response.body = await maxmind.getAsn(context?.params?.ip);
-    } else {
-      context.response.body = {
-        "status": "error",
-      }
-    }
   });
+  // // ip 2 city ipv4
+  // .get("/api/city/:ip", async (context) => {
+  //   if (context?.params?.ip && isIpv4Regex.test(context?.params?.ip)) {
+  //     context.response.body = await maxmind.getLocation(context?.params?.ip);
+  //   } else {
+  //     context.response.body = {
+  //       "status": "error",
+  //     }
+  //   }
+  // })
+  // // ip 2 asn ipv4
+  // .get("/api/asn/:ip", async (context) => {
+  //   if (context?.params?.ip && isIpv4Regex.test(context?.params?.ip)) {
+  //     context.response.body = await maxmind.getAsn(context?.params?.ip);
+  //   } else {
+  //     context.response.body = {
+  //       "status": "error",
+  //     }
+  //   }
+  // });
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
